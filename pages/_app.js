a42f332efee9a3fa073f51d1d1d67f26ff7dafd6
Juwon Hong <juwon2024@gmail.com>
2022-07-21 17:28:21 +0900

    babel modified3

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import "tailwindcss/tailwind.css";
+import React from 'react';
 
 export default function MyApp({ Component, pageProps }) {
   return (
