22292bbec575e5a74a46039b4eb7a22b2e7726e4
Juwon Hong <juwon2024@gmail.com>
2022-07-22 17:45:49 +0900

    config check2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
-module.exports = {  
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],  
-  darkMode: 'media', // or 'media' or 'class'  
-  theme: {      extend: {},  },  
-  variants: {      extend: {},  },  
-  plugins: [],
+module.exports = {
+  plugins: [
+    'tailwindcss',
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
+  ],
 }
+// module.exports = {  
+//   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],  
+//   darkMode: 'media', // or 'media' or 'class'  
+//   theme: {      extend: {},  },  
+//   variants: {      extend: {},  },  
+//   plugins: [],
+// }
 
